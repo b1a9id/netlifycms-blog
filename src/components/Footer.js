3615bca5cee2d9ed55fec32b7c721d0babff3380
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2019-01-13 23:48:52 +0900

    #8 SNSシェアボタン設置

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = class extends React.Component {
+	render() {
+		return (
+			<footer className="text-muted py-5">
+				<div className="container">
+					<p>© 2019 Ryosuke Uchitate</p>
+				</div>
+			</footer>
+		)
+	}
+};
+
+export default Footer
